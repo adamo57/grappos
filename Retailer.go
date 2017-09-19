c9fb32e21600612927eeee3669e386e2fe720c71
adamo57 <adamouellette57@gmail.com>
2017-09-18 22:55:17 -0400

    Added data retriever, breaking apart logic

--- Retailer.go
+++ Retailer.go
@@ -1,14 +1,8 @@
 package grappos
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
 )
-
-var retailerBaseURL = "http://www.grappos.com/api2/retailer.php?1=1&format=json"
 
 type retailer struct {
 	RetailerID  string  `json:"RetailerID"`
@@ -32,65 +26,64 @@ type RetailerAPIResponse struct {
 	Retailers []retailer `json:"retailers"`
 }
 
-func retailerDataRetriever(m *RetailerAPIResponse, q string) error {
-	res, err := http.Get(q)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	err = json.Unmarshal([]byte(body), &m)
-	if err != nil {
-		log.Println("whoops:", err)
-	}
-
-	return err
-}
+var retailerDataRetriever = NewDataRetriever("retailer")
 
 // SearchCoordinates Searches a retailer based of their latitude and longitude.
-func SearchCoordinates(lat string, lon string) (*RetailerAPIResponse, error) {
+func SearchCoordinates(lat string, lon string) (RetailerAPIResponse, error) {
 	var s = new(RetailerAPIResponse)
-	queryString := fmt.Sprintf("&lat=%s&lon=%s", lat, lon)
 
-	err := retailerDataRetriever(s, retailerBaseURL+queryString)
+	m := map[string]string{
+		"lat": lat,
+		"lon": lon,
+	}
+	retailerDataRetriever.addQueryParams(m)
 
-	return s, err
+	err := retailerDataRetriever.getData(s)
+	if err != nil {
+		err = fmt.Errorf("something went wrong: %s", err)
+	}
+
+	return *s, err
 }
 
 // SearchProductByID Searches for a retailer that has a given ProductID.
-func SearchProductByID(id int, st string) (*RetailerAPIResponse, error) {
+func SearchProductByID(id int, st string) (RetailerAPIResponse, error) {
 	var s = new(RetailerAPIResponse)
-	queryString := ""
+	m := map[string]string{}
 
 	if checkStoreType(st) {
-		queryString = fmt.Sprintf("&product_id=%d&store_type=%s", id, st)
+		m["product_id"] = fmt.Sprintf("%d", id)
+		m["store_type"] = st
 	} else {
-		queryString = fmt.Sprintf("&product_id=%d&store_type=All", id)
+		m["product_id"] = fmt.Sprintf("%d", id)
+		m["store_type"] = "All"
 	}
 
-	err := retailerDataRetriever(s, retailerBaseURL+queryString)
+	retailerDataRetriever.addQueryParams(m)
 
-	return s, err
+	err := retailerDataRetriever.getData(s)
+
+	return *s, err
 }
 
 // SearchBrandByID Searches for a retailer that has product(s) with a given BrandID.
-func SearchBrandByID(id int, st string) (*RetailerAPIResponse, error) {
+func SearchBrandByID(id int, st string) (RetailerAPIResponse, error) {
 	var s = new(RetailerAPIResponse)
-	queryString := ""
+	m := map[string]string{}
 
 	if checkStoreType(st) {
-		queryString = fmt.Sprintf("&brand_id=%d&store_type=%s", id, st)
+		m["brand_id"] = fmt.Sprintf("%d", id)
+		m["store_type"] = st
 	} else {
-		queryString = fmt.Sprintf("&brand_id=%d&store_type=All", id)
+		m["brand_id"] = fmt.Sprintf("%d", id)
+		m["store_type"] = "All"
 	}
 
-	err := retailerDataRetriever(s, retailerBaseURL+queryString)
+	retailerDataRetriever.addQueryParams(m)
 
-	return s, err
+	err := retailerDataRetriever.getData(s)
+
+	return *s, err
 }
 
 // CheckStoreType Checks the given input to determine which type of store
