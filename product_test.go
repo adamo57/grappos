a35f77608fc281ed344b81f861f4ec8df372076c
Adam Ouellette <adamouellette57@gmail.com>
2018-02-26 10:37:52 -0500

    removed test keys to get build passing

--- product_test.go
+++ product_test.go
@@ -5,14 +5,14 @@ import (
 )
 
 func TestProduct(t *testing.T) {
-	_, err := GetProducts("Ic-769313195")
+	_, err := GetProducts("")
 	if err != nil {
 		t.Fatal(err)
 	}
 }
 
 func TestBadProduct(t *testing.T) {
-	_, err := GetProducts("IP-0122011238")
+	_, err := GetProducts("")
 	if err == nil {
 		t.Fatal("Should not get in here")
 	}
